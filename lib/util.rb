75ee218590ed93167b8971a441c0e383c7cd0d21
Juanjo Bazán <jjbazan@gmail.com>
2021-03-07 18:00:38 +0100

    add astro-ph query

--- lib/util.rb
+++ lib/util.rb
@@ -1,4 +1,5 @@
 require 'twitter'
+require 'arx'
 
 module Util
 
@@ -6,6 +7,13 @@ module Util
     @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TW_ANDROMEDAFEED_API_KEY"]
       config.consumer_secret     = ENV["TW_ANDROMEDAFEED_SECRET_KEY"]
+    end
+  end
+
+  def astroph_query
+    papers = Arx(sort_by: :date_submitted, sort_order: :descending) do |query|
+      query.category('astro-ph')
+      query.title('Andromeda').or.title('M31')
     end
   end
 
