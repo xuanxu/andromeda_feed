64605d542be45dd816e621ed09eaadf77d8bdea7
Juanjo Bazán <jjbazan@gmail.com>
2021-03-21 13:41:58 +0100

    search all subcategories

--- lib/util.rb
+++ lib/util.rb
@@ -5,13 +5,14 @@ module Util
 
   def twitter_client
     @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TW_ANDROMEDAFEED_API_KEY"]
-      config.consumer_secret     = ENV["TW_ANDROMEDAFEED_SECRET_KEY"]
+      config.consumer_key    = ENV["TW_ANDROMEDAFEED_API_KEY"]
+      config.consumer_secret = ENV["TW_ANDROMEDAFEED_SECRET_KEY"]
     end
   end
 
   def astroph_andromeda_query
-    Arx(sort_by: :date_submitted, sort_order: :descending, max_results: 7) do |query|
+    subcats = ['GA', 'CO', 'EP', 'HE', 'IM', 'SR']
+    Arx(sort_by: :date_submitted, sort_order: :descending) do |query|
       query.category('astro-ph.*')
       query.title('Andromeda Galaxy', 'M31', connective: :or)
     end
