4023a70a62f04655c958d4dd70fc7151c7a8af42
Juanjo Bazán <jjbazan@gmail.com>
2021-03-25 17:27:46 +0100

    get yesterday's papers

--- lib/andromeda_news.rb
+++ lib/andromeda_news.rb
@@ -1,9 +1,10 @@
 require_relative 'util'
+include Util
 
-papers = astroph_andromeda_query
+yesterday_papers = astroph_andromeda_daily_query
 
 papers.each do |paper|
   title = paper.title
   url = paper.url
-  puts "* #{title} (#{url})"
+  # twitter_client send post "#{title}\n#{url}"
 end
