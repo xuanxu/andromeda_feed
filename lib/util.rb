d07a26b77c364004102aaf1ad3059197fd82877c
Juanjo Bazán <jjbazan@gmail.com>
2021-03-25 17:26:56 +0100

    refine query to make it daily

--- lib/util.rb
+++ lib/util.rb
@@ -10,11 +10,14 @@ module Util
     end
   end
 
-  def astroph_andromeda_query
+  def astroph_andromeda_daily_query(day=Date.today.prev_day)
     subcats = ['GA', 'CO', 'EP', 'HE', 'IM', 'SR']
+    all_categories = subcats.map {|subcat| "astro-ph." + subcat}
+    day = day.strftime("%Y%m%d")
     Arx(sort_by: :date_submitted, sort_order: :descending) do |query|
-      query.category('astro-ph.*')
+      query.category(all_categories, connective: :or)
       query.title('Andromeda Galaxy', 'M31', connective: :or)
+      query.last_updated_date("[#{yesterday}0000 TO #{yesterday}2359]", exact: false)
     end
   end
 
