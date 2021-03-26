61d75e646699c299492b86c4d8dba3f61014cc63
Juanjo Bazán <jjbazan@gmail.com>
2021-03-26 10:48:09 +0100

    add emojis

--- lib/andromeda_news.rb
+++ lib/andromeda_news.rb
@@ -2,9 +2,11 @@ require_relative 'util'
 include Util
 
 yesterday_papers = astroph_andromeda_daily_query
+emojis = %w(✨ 🛰 🌠 💫 🚀 🌌 💥 ⚡️ 📃 🔭 🌟)
+
 
 papers.each do |paper|
   title = paper.title
   url = paper.url
-  # twitter_client send post "#{title}\n#{url}"
+  # twitter_client send post "#{emojis.sample} #{title}\n#{url}"
 end
