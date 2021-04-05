fe5e763b04dbf556ff589fb4f197cb337c0fea1a
Juanjo Bazán <jjbazan@gmail.com>
2021-04-05 20:30:16 +0200

    tweet APOD if it mentions Andromeda in the title

--- lib/andromeda_feed.rb
+++ lib/andromeda_feed.rb
@@ -3,13 +3,20 @@ require_relative 'util'
 class AndromedaFeed
   include Util
 
-  def update
+  def update_papers
     yesterday_papers = astroph_andromeda_daily_query
 
     papers.each do |paper|
       title = paper.title
       url = paper.url
       twitter_client.update("#{emojis.sample} #{title}\n#{url}")
+    end
+  end
+
+  def update_apod
+    pic = apod_today
+    if pic.title.to_s.include? "Andromeda"
+      twitter_client.update("#{pic.title} #{apod_url_today}")
     end
   end
 
